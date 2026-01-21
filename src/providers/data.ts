d20f21778c887d09303efdd603d393cb081e9475
jmisf14 <jmisf14@gmail.com>
2026-01-21 10:50:26 +0100

    data routing

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,5 +1,31 @@
 import { createSimpleRestDataProvider } from "@refinedev/rest/simple-rest";
+import type {
+  BaseRecord,
+  DataProvider,
+  GetListParams,
+  GetListResponse,
+} from "@refinedev/core";
 import { API_URL } from "./constants";
-export const { dataProvider, kyInstance } = createSimpleRestDataProvider({
+
+const { dataProvider: restDataProvider, kyInstance } = createSimpleRestDataProvider({
   apiURL: API_URL,
 });
+
+export { kyInstance };
+
+export const dataProvider: DataProvider = {
+  ...restDataProvider,
+  getList: async <TData extends BaseRecord = BaseRecord>(
+    params: GetListParams,
+  ): Promise<GetListResponse<TData>> => {
+    const { resource } = params;
+
+    // Match your screenshot's intent: only support "subjects" for now.
+    if (resource !== "subjects") {
+      return { data: [] as TData[], total: 0 };
+    }
+
+    // Delegate to the REST provider for real data.
+    return await restDataProvider.getList<TData>(params);
+  },
+};
