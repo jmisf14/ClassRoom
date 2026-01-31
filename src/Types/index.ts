f1cc453462e98137f022b88bccbd89f841dc901e
jmisf14 <jmisf14@gmail.com>
2026-01-31 12:40:20 +0100

    index

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -1,8 +1,40 @@
+export type Department = {
+    id: number;
+    code: string;
+    name: string;
+    description: string | null;
+    createdAt: string;
+    updatedAt: string;
+};
+
 export type Subject = {
     id: number;
+    departmentId: number;
     name: string;
     code: string;
-    description: string;
-    department: string;
+    description: string | null;
     createdAt: string;
-}
+    updatedAt: string;
+    department: Department;
+};
+
+export type Pagination = {
+    page: number;
+    limit: number;
+    total: number;
+    totalPages: number;
+};
+
+export type ApiListResponse<T> = {
+    data: T[];
+    pagination: Pagination;
+};
+
+export type SignupPayload = {
+    email: string;
+    password: string;
+    image?: string;
+    imageCldPubId?: string;
+    role: "student" | "teacher" | "admin";
+
+};
