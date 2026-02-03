460ed3322b9fd93249ef76c2f5b7af9a6bd63039
jmisf14 <jmisf14@gmail.com>
2026-02-03 18:42:24 +0100

    correction

--- src/pages/classes/classes.tsx
+++ src/pages/classes/classes.tsx
@@ -1,7 +1,9 @@
 import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb";
 import { CreateView } from "@/components/refine-ui/views/create-view";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useBack } from "@refinedev/core";
+import { Separator } from "@/components/ui/separator";
 
 const Create = () => {
     const back = useBack();
@@ -17,6 +19,24 @@ const Create = () => {
                 <Button type="button" onClick={() => back()}>
                     Go Back
                 </Button>
+            </div>
+
+            <Separator />
+
+            <div className="my-4 flex items-center">
+                <Card className="class-form-card">
+                    <CardHeader className="relative z-10">
+                        <CardTitle className="text-2xl pb-0 font-bold">
+                            Fill out the form
+                        </CardTitle>
+                    </CardHeader>
+
+                    <Separator />
+
+                    <CardContent className="mt-7">
+                        {/* TODO: form fields will go here */}
+                    </CardContent>
+                </Card>
             </div>
         </CreateView>
     );
