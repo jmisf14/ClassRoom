4437292b808381305f0dfc2eafd95c8d2f3627f3
jmisf14 <jmisf14@gmail.com>
2026-02-03 18:32:43 +0100

    classes.tx

--- src/pages/classes/classes.tsx
+++ src/pages/classes/classes.tsx
@@ -1,9 +1,25 @@
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb";
+import { CreateView } from "@/components/refine-ui/views/create-view";
+import { Button } from "@/components/ui/button";
+import { useBack } from "@refinedev/core";
+
 const Create = () => {
+    const back = useBack();
+
     return (
-        <div>
-            Create
-        </div>
-    )
-}
+        <CreateView className="class-view">
+            <Breadcrumb />
+
+            <h1 className="page-title">Create a Class</h1>
+
+            <div className="intro-row">
+                <p>Provide the required information below to add a class.</p>
+                <Button type="button" onClick={() => back()}>
+                    Go Back
+                </Button>
+            </div>
+        </CreateView>
+    );
+};
 
 export default Create;
