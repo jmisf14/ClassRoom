f71e9ffd2859179f4610c3c5a98c295c1ae1dae6
jmisf14 <jmisf14@gmail.com>
2026-02-03 18:00:30 +0100

    changes in the routes

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,10 @@ import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import Dashboard from "./pages/dashboard";
 import SubjectsList from "./pages/subjects/list";
 import SubjectsCreate from "./pages/subjects/create";
+import ClassesList from "./pages/classes/lists";
+import ClassesCreate from "./pages/classes/classes";
 import { Layout } from "./components/refine-ui/layout/layout";
-import { BookOpen, Home } from "lucide-react";
+import { BookOpen, GraduationCap } from "lucide-react";
 import { dataProvider } from "@/providers/data.ts";
 
 function App() {
@@ -50,16 +52,17 @@ function App() {
               }}
               resources={[
                 {
-                  name: 'dashboard',
-                  list: '/',
-                  meta: { label: 'Home', icon: <Home /> }
-
-                },
-                {
                   name: 'subjects',
                   list: '/subjects',
                   create: '/subjects/create',
                   meta: { label: 'Subjects', icon: <BookOpen /> }
+
+                },
+                {
+                  name: 'classes',
+                  list: '/classes',
+                  create: '/classes/create',
+                  meta: { label: 'Classes', icon: <GraduationCap/> }
                 }
               ]}
             >
@@ -75,6 +78,10 @@ function App() {
                 <Route path="subjects">
                   <Route index element={<SubjectsList />} />
                   <Route path="create" element={<SubjectsCreate />} />
+                </Route>
+                <Route path="classes">
+                  <Route index element={<ClassesList />} />
+                  <Route path="create" element={<ClassesCreate />} />
                 </Route>
               </Routes>
               <Toaster />
